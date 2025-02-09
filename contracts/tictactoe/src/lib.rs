9b479452f9a64bdc4ba7907a794d04199668fa22
Evan Hallmark <ehallmar@usc.edu>
2025-02-08 22:01:08 -0800

    tic tac toe

---
+++ contracts/tictactoe/src/lib.rs
@@ -0,0 +1,159 @@
+#![no_std]
+use soroban_sdk::{contract, contractimpl, log, contracterror, bytes, symbol_short, Env, Address, Symbol, Bytes, String};
+
+#[contract]
+pub struct Contract;
+
+const X: Symbol = symbol_short!("X");
+const O: Symbol = symbol_short!("O");
+const NULL: Symbol = symbol_short!("NULL");
+
+const PLAYER: Symbol = symbol_short!("PLAYER");
+const OPPONENT: Symbol = symbol_short!("OPPONENT");
+const PLAYING: Symbol = symbol_short!("PLAYING");
+const NEXT: Symbol = symbol_short!("NEXT");
+const WINNER: Symbol = symbol_short!("WINNER");
+
+#[contractimpl]
+impl Contract {
+    pub fn start(env: Env, player_addr: Address, opponent_addr: Address) -> u32 {
+        player_addr.require_auth();
+
+        let mut playing: u32 = env.storage().instance().get(&PLAYING).unwrap_or(0);
+        log!(&env, "playing", &playing);
+        if playing == 0 {
+            log!(&env, "not playing");
+            let empty: String = String::from_str(&env, "");
+            env.storage().instance().set(&PLAYER, &player_addr.to_string());
+            env.storage().instance().set(&OPPONENT, &opponent_addr.to_string());
+            env.storage().instance().set(&NEXT, &player_addr.to_string());
+            env.storage().instance().set(&WINNER, &empty);
+            for i in 0..9u32 {
+                env.storage().instance().set(&i, &NULL);
+            }
+            // update state to "playing"
+            playing += 1;            
+            env.storage().instance().set(&PLAYING, &playing);
+            env.storage().instance().extend_ttl(100, 100);
+            0
+        } else {               
+            log!(&env, "playing");
+            1
+        }
+    }
+
+    pub fn play(env: Env, addr: Address, player_move: u32) -> u32 {
+        log!(&env, "play...");
+        addr.require_auth();
+        
+        log!(&env, "pre assert...");
+
+        let mut playing: u32 = env.storage().instance().get(&PLAYING).unwrap_or(0);
+
+        if playing == 0 {
+            log!(&env, "not playing!");
+            return 1;
+        }
+
+        log!(&env, "post assert...");
+        
+        let empty: String = String::from_str(&env, "");
+
+        // make sure player is next
+        if env.storage().instance().get(&NEXT).unwrap_or(empty.clone()) != addr.to_string() {
+            // bad
+            log!(&env, "user is not next to play");
+            return 1;
+        }
+
+        // check if player is playing
+        let mut value: Symbol = NULL;
+        let mut player = env.storage().instance().get(&PLAYER).unwrap_or(empty.clone());
+        let mut opponent = env.storage().instance().get(&OPPONENT).unwrap_or(empty.clone());
+        let mut next = String::from_str(&env, "");
+
+
+        if player == addr.to_string() {
+            log!(&env, "is player");
+            next = opponent;
+            value = X;
+        } else if opponent == addr.to_string() {
+            log!(&env, "is opponent");
+            next = player;
+            value = O;
+        } else {
+            // bad case!
+            log!(&env, "BAD!");
+            return 1;
+        }
+
+        log!(&env, "player move: {}", &player_move);
+
+        if player_move >= 0 && player_move < 9 {
+            // valid move
+            if env.storage().instance().get(&player_move).unwrap_or(NULL) != NULL {
+                log!(&env, "someone already played this tile");
+                return 1;
+            }
+        } else {
+            return 1;
+        }
+
+        env.storage().instance().set(&player_move, &value);
+        env.storage().instance().set(&NEXT, &next);
+        env.storage().instance().extend_ttl(100, 100);
+
+        // check for winner
+        let r = player_move / 3;
+        let c = player_move % 3;
+
+        // check rows and cols
+        let mut win: bool = false;
+        for i in 0..3u32 {
+            if env.storage().instance().get(&(i*3)).unwrap_or(NULL) == value && env.storage().instance().get(&(i*3 + 1)).unwrap_or(NULL) == value && env.storage().instance().get(&(i*3 + 2)).unwrap_or(NULL) == value {
+                win = true;
+                break;
+            }
+            if env.storage().instance().get(&(i)).unwrap_or(NULL) == value && env.storage().instance().get(&(1*3u32 + i)).unwrap_or(NULL) == value && env.storage().instance().get(&(2*3 + i)).unwrap_or(NULL) == value {
+                win = true;
+                break;
+            }
+        }
+
+        // check diagonals
+        if !win {
+            if env.storage().instance().get(&0u32).unwrap_or(NULL) == value && env.storage().instance().get(&4u32).unwrap_or(NULL) == value && env.storage().instance().get(&8u32).unwrap_or(NULL) == value {
+                win = true;
+            }
+            if env.storage().instance().get(&0u32).unwrap_or(NULL) == value && env.storage().instance().get(&4u32).unwrap_or(NULL) == value && env.storage().instance().get(&8u32).unwrap_or(NULL) == value {
+                win = true;
+            }
+        }
+
+        if win {
+            env.storage().instance().set(&WINNER, &addr.to_string());
+            env.storage().instance().set(&PLAYING, &0u32);        
+        }
+
+        log!(&env, "Board:");
+        log!(&env, "", env.storage().instance().get(&0u32).unwrap_or(NULL),
+                               env.storage().instance().get(&1u32).unwrap_or(NULL),
+                               env.storage().instance().get(&2u32).unwrap_or(NULL));
+        log!(&env, "", env.storage().instance().get(&3u32).unwrap_or(NULL),
+                               env.storage().instance().get(&4u32).unwrap_or(NULL),
+                               env.storage().instance().get(&5u32).unwrap_or(NULL));
+        log!(&env, "", env.storage().instance().get(&6u32).unwrap_or(NULL),
+                               env.storage().instance().get(&7u32).unwrap_or(NULL),
+                               env.storage().instance().get(&8u32).unwrap_or(NULL));
+        log!(&env, "------------");
+        0
+    }
+
+    pub fn winner(env: Env) -> String {
+        let empty: String = String::from_str(&env, "");
+        log!(&env, "", env.storage().instance().get(&WINNER).unwrap_or(empty.clone()));
+        env.storage().instance().get(&WINNER).unwrap_or(empty)
+    }
+}
+
+mod test;
---
+++ contracts/tictactoe/src/test.rs
@@ -0,0 +1,114 @@
+#![cfg(test)]
+extern crate std;
+
+use super::*;
+use soroban_sdk::{
+    symbol_short,
+    testutils::{Address as _, AuthorizedFunction, AuthorizedInvocation},
+    Address, Env, IntoVal, String
+};
+
+#[test]
+fn test() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let contract_id = env.register(Contract, ());
+    let client = ContractClient::new(&env, &contract_id);
+
+    let alice = Address::generate(&env);
+    let bob = Address::generate(&env);
+
+    // alice start's the game
+    assert_eq!(
+        client.start(&alice, &bob),
+        0
+    );
+
+    // verify signature
+    assert_eq!(
+        env.auths(),
+        std::vec![(
+            // Address for which authorization check is performed
+            alice.clone(),
+            // Invocation tree that needs to be authorized
+            AuthorizedInvocation {
+                // Function that is authorized. Can be a contract function or
+                // a host function that requires authorization.
+                function: AuthorizedFunction::Contract((
+                    // Address of the called contract
+                    contract_id.clone(),
+                    // Name of the called function
+                    symbol_short!("start"),
+                    // Arguments used to call `increment` (converted to the env-managed vector via `into_val`)
+                    (alice.clone(), bob.clone()).into_val(&env),
+                )),
+                // The contract doesn't call any other contracts that require
+                // authorization,
+                sub_invocations: std::vec![]
+            }
+        )]
+    );
+
+    // starting game again should fail
+    assert_eq!(
+        client.start(&alice, &bob),
+        1
+    
+    );
+    
+    // alice goes middle
+    assert_eq!(
+        client.play(&alice, &4),
+        0
+    );
+
+    // bob can't also go center center
+    assert_eq!(
+        client.play(&bob, &4),
+        1
+    );
+
+    // bob can't go off board
+    assert_eq!(
+        client.play(&bob, &9),
+        1
+    );
+
+    // bob goes top left
+    assert_eq!(
+        client.play(&bob, &0),
+        0
+    );
+
+    // alice goes top center
+    assert_eq!(
+        client.play(&alice, &1),
+        0
+    );
+
+
+    // bob goes center left
+    assert_eq!(
+        client.play(&bob, &3),
+        0
+    );
+
+    // alice goes bottom center (winning the game)
+    assert_eq!(
+        client.play(&alice, &7),
+        0
+    );
+
+    // alice is the winner
+    assert_eq!(
+        client.winner(),
+        alice.to_string()        
+    );
+
+    // bob goes center right should fail since game is over
+    assert_eq!(
+        client.play(&bob, &5),
+        1
+    );
+}
